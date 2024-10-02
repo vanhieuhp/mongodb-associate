a35a756c36683b7cb9e7c33b4efc4c53a33c609e
vanhieu <vanhieuit10@gmail.com>
2024-10-03 05:26:54 +0700

    first commit

---
+++ develop/faker.js
@@ -0,0 +1,19 @@
+const {faker} = require('@faker-js/faker');
+const users = [];
+for (let i = 0; i < 100000; i++) {
+    users.push({
+        name: faker.person.fullName(),
+        age: faker.number.int({ min: 18, max: 60 }),
+        email: faker.internet.email(),
+        phone: faker.phone.number(),
+        address: faker.location.streetAddress(),
+        city: faker.location.city(),
+        state: faker.location.state(),
+        zip: faker.location.zipCode(),
+        country: faker.location.country(),
+        company: faker.company.name(),
+        isActivated: faker.datatype.boolean(),
+    });
+}
+
+db.getSiblingDB('test').users.insertMany(users);
---
+++ develop/help-mongo.js
@@ -0,0 +1,34 @@
+// Usage: mongo --shell help-mongo.js
+
+// Config Replica Set
+rs.initiate(
+    {
+        _id: "replicaset",
+        version: 1,
+        members: [
+            {_id: 0, host: "mongo1"},
+            {_id: 1, host: "mongo2"},
+            {_id: 2, host: "mongo3"}
+        ]
+    }
+)
+config.members[0].priority = 10
+// openssl
+// rand - base64
+// 756 > config / mongod - keyfile
+
+// Create User
+db.createUser({
+    user: "dba-admin",
+    pwd: "dba-pass",
+    roles: [
+        {role: "root", db: "admin"}
+    ]
+})
+
+
+// Indexes
+db.users.explain('queryPlanner').find({age: {$gt: 25}, isActivated: true})
+db.users.find({age: {$gt: 25}, isActivated: true}).limit(3)
+db.users.find({age: {$gt: 25}, isActivated: true}).explain().queryPlanner.winningPlan
+db.users.find({age: {$gt: 25}, isActivated: true}).explain().queryPlanner.rejectedPlan
